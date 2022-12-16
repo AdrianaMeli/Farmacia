f0edab1c91b5f63ec64ef9b45945167a9e3ccdbf
AdrianaMeli <adriana.nogueira@mercadolivre.com>
2022-12-16 17:21:04 -0300

    atualizando repository

--- src/main/java/com/generation/farmacia/Repository/CategoriaRepository.java
+++ src/main/java/com/generation/farmacia/Repository/CategoriaRepository.java
@@ -4,9 +4,10 @@ package com.generation.farmacia.Repository;
 import com.generation.farmacia.Model.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
     public List<Categoria> findAllByCategoriaContainingIgnoreCase(@Param("tipo") String tipo);
 
